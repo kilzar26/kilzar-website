61922c82a23a1719c46ee4427bc6cdc88a6ea18a
kilzar26 <ahmad.kilzar@gmail.com>
2026-08-06 15:19:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+KILZAR TECHNOLOGIES WEBSITE
+
+No installation is required.
+
+To preview:
+1. Open the folder.
+2. Double-click index.html.
+
+To publish free:
+Upload this folder to Cloudflare Pages, Netlify, or GitHub Pages.
+Custom domain: kilzartech.com
+Contact email: info@kilzartech.com
